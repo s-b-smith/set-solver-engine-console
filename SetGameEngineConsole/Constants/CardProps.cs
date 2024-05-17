6bcfd64bf04a303d0479e67409fa3d595d5b73fa
Skylar Smith <Skylar Smith@DESKTOP-BBL73PD>
2024-05-17 16:38:34 -0500

    Add project files.

---
+++ SetGameEngineConsole/Constants/CardProps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetSolverEngineConsole.Constants
+{
+    internal static class CardProps
+    {
+        public enum CardProp
+        {
+            COLOR,
+            SHAPE,
+            NUM,
+            SHADING,
+        };
+
+        public enum COLOR 
+        {
+            RED,
+            GREEN,
+            PURPLE,
+        };
+
+        public enum SHAPE
+        {
+            CIRCLE,
+            DIAMOND,
+            SQUIGGLE,
+        }
+
+        public enum NUM
+        {
+            ONE,
+            TWO,
+            THREE,
+        }
+
+        public enum SHADING
+        {
+            EMPTY,
+            SHADED,
+            SOLID,
+        }
+    }
+}
---
+++ SetGameEngineConsole/Models/SetSolverResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetSolverEngineConsole.Models
+{
+    internal class SetSolverResult(int numSets, Set[] sets)
+    {
+        public readonly int numSets = numSets;
+        public readonly Set[] sets = sets;
+    }
+}
