88e4f9e00d731d6307280784e7c7d8844d7b7095
Skylar Smith <s.b.smith97@gmail.com>
2024-06-15 12:25:00 -0500

    Prompt strucuture created. Working on input service to validate and process user input

--- SetSolverEngineConsole/Program.cs
+++ SetSolverEngineConsole/Program.cs
@@ -1,25 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SetSolverEngineConsole.Models;
+﻿using SetSolverEngineConsole.Models;
 using SetSolverEngineConsole.Services;
+using System.Text;
 using static SetSolverEngineConsole.Constants.CardProps;
 
-Card[] cards = [
-    new Card(COLOR.RED, SHAPE.OVAL, NUM.ONE, SHADING.EMPTY),
-    new Card(COLOR.RED, SHAPE.SQUIGGLE, NUM.ONE, SHADING.EMPTY),
-    new Card(COLOR.RED, SHAPE.DIAMOND, NUM.ONE, SHADING.EMPTY),
-    new Card(COLOR.RED, SHAPE.OVAL, NUM.TWO, SHADING.EMPTY),
-    new Card(COLOR.PURPLE, SHAPE.DIAMOND, NUM.TWO, SHADING.SHADED),
-    new Card(COLOR.GREEN, SHAPE.SQUIGGLE, NUM.THREE, SHADING.SOLID),
-    new Card(COLOR.RED, SHAPE.OVAL, NUM.THREE, SHADING.EMPTY),
-    new Card(COLOR.GREEN, SHAPE.OVAL, NUM.ONE, SHADING.SOLID),
-    new Card(COLOR.PURPLE, SHAPE.OVAL, NUM.ONE, SHADING.SHADED),
-    new Card(COLOR.RED, SHAPE.SQUIGGLE, NUM.THREE, SHADING.EMPTY),
-    new Card(COLOR.PURPLE, SHAPE.DIAMOND, NUM.THREE, SHADING.SHADED),
-    new Card(COLOR.GREEN, SHAPE.SQUIGGLE, NUM.THREE, SHADING.EMPTY),
-];
+const string FINISHED = "F";
+
+//Card[] cards = [
+//    new Card(COLOR.RED, SHAPE.OVAL, NUM.ONE, SHADING.EMPTY),
+//    new Card(COLOR.RED, SHAPE.SQUIGGLE, NUM.ONE, SHADING.EMPTY),
+//    new Card(COLOR.RED, SHAPE.DIAMOND, NUM.ONE, SHADING.EMPTY),
+//    new Card(COLOR.RED, SHAPE.OVAL, NUM.TWO, SHADING.EMPTY),
+//    new Card(COLOR.PURPLE, SHAPE.DIAMOND, NUM.TWO, SHADING.SHADED),
+//    new Card(COLOR.GREEN, SHAPE.SQUIGGLE, NUM.THREE, SHADING.SOLID),
+//    new Card(COLOR.RED, SHAPE.OVAL, NUM.THREE, SHADING.EMPTY),
+//    new Card(COLOR.GREEN, SHAPE.OVAL, NUM.ONE, SHADING.SOLID),
+//    new Card(COLOR.PURPLE, SHAPE.OVAL, NUM.ONE, SHADING.SHADED),
+//    new Card(COLOR.RED, SHAPE.SQUIGGLE, NUM.THREE, SHADING.EMPTY),
+//    new Card(COLOR.PURPLE, SHAPE.DIAMOND, NUM.THREE, SHADING.SHADED),
+//    new Card(COLOR.GREEN, SHAPE.SQUIGGLE, NUM.THREE, SHADING.EMPTY),
+//];
 
 var solverService = new SolverService();
-var solveResult = solverService.FindSets(cards);
+//var solveResult = solverService.FindSets(cards);
 
-Console.WriteLine(solveResult.ToString());
-Console.WriteLine("ALL DONE!");
+Console.WriteLine(getOpenTitle());
+
+List<Card> cards = new();
+do
+{
+    Console.WriteLine(getPrompt());
+} while (Console.ReadLine() != FINISHED);
+
+//Console.WriteLine(solveResult.ToString());
+//Console.WriteLine("ALL DONE!");
+
+string getOpenTitle()
+{
+    StringBuilder sb = new();
+
+    sb.AppendLine("|||||||||||||||||||");
+    sb.AppendLine("|||||||||||||||||||");
+    sb.AppendLine("|||||-- SET --|||||");
+    sb.AppendLine("||||-- SOLVER --|||");
+    sb.AppendLine("|||||||||||||||||||");
+    sb.AppendLine();
+    sb.AppendLine("*******************");
+    sb.AppendLine();
+    sb.AppendLine("WELCOME!");
+    sb.AppendLine();
+    sb.AppendLine("Enter \"h\" for help");
+
+    return sb.ToString();
+}
+
+string getPrompt()
+{
+    return $"Enter card(s), or \"{FINISHED}\" if finished:";
+}
---
+++ SetSolverEngineConsole/Services/InputService.cs
@@ -0,0 +1,33 @@
+﻿using SetSolverEngineConsole.Models;
+
+namespace SetSolverEngineConsole.Services
+{
+    public class InputService
+    {
+        public Card readCard(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string validateCardInput(string cardInput)
+        {
+            if (cardInput.Length != 4)
+            {
+                if (cardInput.Length > 4)
+                {
+                    return "Too many characters!";
+                }
+                else
+                {
+                    return "Are you missing input?";
+                }
+            }
+
+            // TODO: Check that one of each props is listed
+            // Remember that you want them to enter as many cards as they want (within reason)
+            // Space-separated
+
+            throw new NotImplementedException();
+        }
+    }
+}
